31f79b667878b0415fb9180a6a6d3a9180974bb2
Tom Zaragoza <tomdzaragoza@gmail.com>
2014-03-01 14:22:09 -0500

    add in showing that graph is being loaded

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,15 @@
 $("#createGraphIndex").click(function(e) {
-	$("#loading").html("<p>Creating an empty graph. You'll be redirected to it...</p>");
+	$(this).attr("disabled", true);
+	$(this).html("Redirecting...");
+	$("#loading").html("<br><img src='/static/img/loadingrays.gif' alt='loading...'></img>");
 	$.ajax({
 				type: "POST",
 				url: "/nonregistered_create_graph",
 				success: function(d) {
 					if (d.created) {
-						console.log("success");
-						console.log(d.url);
-
-						window.location = "http://localhost:5000/graph_nonregistered/" + d.url;
+						window.location = "https://graphbin.co/graph_nonregistered/" + d.url;
 					} else if (d.created === false) {
-						window.location = "http://localhost:5000/";
+						window.location = "https://graphbin.co/";
 					}
 				},
 				error: function(e) {
